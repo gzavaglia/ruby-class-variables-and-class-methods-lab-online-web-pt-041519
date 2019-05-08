15ada552b00c85b86f89785b89f0bb233cb78310
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-05-08 03:39:22 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,47 @@
+class Song
+  @@count = 0 
+  @@genres = []
+  @@artists = []
+  @@artists2 = []
+  @@genres2 = []
+  attr_accessor :name, :artist, :genre 
+  def initialize(song_title, song_artist, song_genre)
+    @name = song_title
+    @artist = song_artist
+    @genre = song_genre
+    @@count += 1 
+        @@artists << song_artist
+      if @@artists2.include?(song_artist) == false
+        @@artists2 << song_artist
+      end
+      @@genres << song_genre
+      if @@genres2.include?(song_genre) == false
+        @@genres2 << song_genre
+      end 
+      
+  end 
+  
+  def self.count 
+    return @@count
+  end 
+  
+  def self.artists
+    return @@artists2
+  end 
+  
+  def self.genres
+    return @@genres2
+  end
+  
+  def self.genre_count
+    i = 0 
+    @@genres.each do |gen|
+      
+    end 
+  end
+  
+  def self.artist_count
+    
+  end
+  
+end 
